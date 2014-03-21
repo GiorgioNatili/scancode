c115b0fef32aecb55243a06f14e15e0c564441c6
GiorgioNatili <g.natili@gnstudio.com>
2014-03-21 18:58:01 +0100

    Sample Implementation

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,25 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.phonegap.plugins.barcodescanner/www/barcodescanner.js",
+        "id": "com.phonegap.plugins.barcodescanner.BarcodeScanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.phonegap.plugins.barcodescanner": "1.0.1",
+    "org.apache.cordova.dialogs": "0.2.6"
+}
+// BOTTOM OF METADATA
+});
